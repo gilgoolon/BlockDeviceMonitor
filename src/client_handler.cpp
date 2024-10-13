06cdca930182b6322d9a7f67f2efc73d91730f39
gilgoolon <alpertgil@gmail.com>
2024-10-13 11:02:19 +0300

    implement ClientHandler to support current client requests

--- src/client_handler.cpp
+++ src/client_handler.cpp
@@ -1,3 +1,5 @@
+#include "proto/server_message.pb.h"
+
 #include "client_handler.hpp"
 
 ClientHandler::ClientHandler(std::shared_ptr<Client> client, std::shared_ptr<std::vector<Rule>> rules)
@@ -8,5 +10,50 @@ ClientHandler::ClientHandler(std::shared_ptr<Client> client, std::shared_ptr<std
 void ClientHandler::handle_indefinitely()
 {
     std::cout << __FUNCTION__ << " handle client commands loop" << std::endl;
-    sleep(1000);
+    while (true)
+    {
+        auto client_message = _client->receive();
+        switch (client_message.type())
+        {
+        case ADD_RULE:
+            handle_add_rule(client_message);
+            break;
+
+        case REMOVE_RULE:
+            handle_remove_rule(client_message);
+            break;
+
+        case GET_EXISTING_RULES:
+            handle_get_existing_rules(client_message);
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+
+void ClientHandler::handle_add_rule(ClientMessage &client_message)
+{
+    AddRuleClientMessageContent add_rule_content;
+    client_message.content().UnpackTo(&add_rule_content);
+    _rules->push_back(add_rule_content.rule());
+}
+
+void ClientHandler::handle_remove_rule(ClientMessage &client_message)
+{
+    RemoveRuleClientMessageContent remove_rule_content;
+    client_message.content().UnpackTo(&remove_rule_content);
+    _rules->erase(_rules->begin() + remove_rule_content.rule_index());
+}
+
+void ClientHandler::handle_get_existing_rules(ClientMessage &client_message)
+{
+    ExistingRulesServerMessageContent content;
+    for (auto &rule : *_rules)
+    {
+        content.add_rules()->CopyFrom(rule);
+    }
+    ServerMessage message;
+    message.mutable_content()->PackFrom(content);
 }
--- src/client_handler.hpp
+++ src/client_handler.hpp
@@ -1,4 +1,5 @@
 #include "client.hpp"
+#include "proto/rule.pb.h"
 
 class ClientHandler final
 {
@@ -10,4 +11,10 @@ public:
 private:
     std::shared_ptr<Client> _client;
     std::shared_ptr<std::vector<Rule>> _rules;
+
+    void handle_add_rule(ClientMessage &client_message);
+
+    void handle_remove_rule(ClientMessage &client_message);
+
+    void handle_get_existing_rules(ClientMessage &client_message);
 };
