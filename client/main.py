de2f799f43e50b330dfc644de4830926183e3b7e
gilgoolon <alpertgil@gmail.com>
2024-10-14 16:30:44 +0300

    move client proto files to 'proto' folder

--- client/main.py
+++ client/main.py
@@ -1,5 +1,5 @@
 import socket
-import server_message_pb2
+import proto.server_message_pb2 as server_message
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
@@ -11,11 +11,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
         if not data:
             break
 
-        message = server_message_pb2.ServerMessage()
+        message = server_message.ServerMessage()
         message.ParseFromString(data)
 
-        if message.content.Is(server_message_pb2.ReportEventServerMessageContent.DESCRIPTOR):
-            content = server_message_pb2.ReportEventServerMessageContent()
+        if message.content.Is(server_message.ReportEventServerMessageContent.DESCRIPTOR):
+            content = server_message.ReportEventServerMessageContent()
             message.content.Unpack(content)
             event = content.event
             print(f"Type={message.type}, Event: Action={event.action}, Name={event.devname}")
