e8426463837d06e896e902e8aee7e1a208f72064
gilgoolon <alpertgil@gmail.com>
2024-10-15 13:47:35 +0300

    cleans

--- src/block_device/netlink.cpp
+++ src/block_device/netlink.cpp
@@ -4,18 +4,19 @@
 #include "../common/os_utils.hpp"
 #include "netlink.hpp"
 
-static constexpr size_t NETLINK_BROADCAST = 1;
-
 std::shared_ptr<Socket> Netlink::make_netlink_uevent_socket()
 {
     auto auto_sock = std::make_unique<Socket>(OS::covered_call(::socket, AF_NETLINK, SOCK_RAW, NETLINK_KOBJECT_UEVENT));
 
+    static constexpr uint32_t NETLINK_BROADCAST = 1;
+    static constexpr uint32_t NETLINK_ADDRESS_FORMAT = AF_NETLINK;
+
     struct sockaddr_nl sa;
-    memset(&sa, 0, sizeof(sa));
-    sa.nl_family = AF_NETLINK;
+    std::memset(&sa, 0, sizeof(sa));
+    sa.nl_family = NETLINK_ADDRESS_FORMAT;
     sa.nl_pid = getpid();
     sa.nl_groups = NETLINK_BROADCAST;
     OS::covered_call(::bind, auto_sock->get_socket_fd(), reinterpret_cast<sockaddr*>(&sa), sizeof(sa));
 
-    return std::move(auto_sock);
+    return auto_sock;
 }
